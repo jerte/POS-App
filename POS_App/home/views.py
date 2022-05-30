11bce0f8b97942c7a6d5802a38e0b8cf71939024
Josh Thomas <joshrthomas@protonmail.com>
2022-05-30 18:18:36 -0400

    created basic add user functionality

--- POS_App/home/views.py
+++ POS_App/home/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
-
+@login_required
 def index(request):
 	return HttpResponse('This app will be used for startup nagivation')
 
 def login(request):
 	return HttpResponse('sadie todo')
+
+def create_user(request):
+	if request.method=='POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			raw_password = form.cleaned_data.get('password1')
+			user = authenticate(username=username, password=raw_password)
+			auth_login(request, user)
+			return redirect('index')
+		else:
+			return render(request, 'add_user.html', {'form':form})
+	else:
+		form = UserCreationForm()
+		return render(request, 'add_user.html', {'form':form})
